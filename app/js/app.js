12dfe79d79b1249ea736ee417e21e011a111499a
Laura <flaura42@gmail.com>
2020-07-10 16:48:49 -0700

    add owm data to page

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,35 @@ const link = 'http://api.openweathermap.org/data/2.5/weather?zip=98133&units=imp
 
 async function getWeather() {
   try {
+    console.log('trying');
     const response = await getData();
-    console.log('response: ', response);
+    console.log(response);
+    const data = {
+      city: `City: ${response.name}`,
+      temp: `Current Temperature: ${Math.round(response.main.temp)}`,
+      feel: `Feels Like: ${Math.round(response.main.feels_like)}`,
+      desc: `Described as: ${response.weather[0].description}`,
+      icon: response.weather[0].icon
+    };
+    const section = document.getElementById('results');
+    const div = document.createElement('div');
+
+    for (let i=0; i<4; i++) {
+      const key = Object.keys(data)[i];
+      const info = Object.entries(data)[i][1];
+      console.log(key, info);
+      const p = document.createElement('p');
+      p.className = `results-${key}`;
+      p.innerHTML = info;
+      div.appendChild(p);
+    }
+    const icon = Object.entries(data)[4][1];
+    console.log(icon);
+    const img = document.createElement('img');
+    img.className = 'results-icon';
+    img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    div.appendChild(img);
+    section.appendChild(div);
   } catch(e) {
     console.log('error is: ', e);
   }
